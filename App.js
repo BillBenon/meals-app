0d467505d383079b0f0e05d92b789264482ac6e4
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-01-08 22:36:50 +0200

    adding header buttons

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
-import { enableScreens } from 'react-native-screens';
+import { enableScreens } from "react-native-screens";
 
 import MealsNavigator from "./navigation/MealsNavigator";
 
